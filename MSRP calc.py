f48157e1a74666ad2e32d2af4d67d0f9b985d889
Wise_XXIV <dz.diego5967@outlook.com>
2022-01-19 12:04:21 -0500

    MSRP Calc
    was told to come up with a retail markup calculator, so here is what ive pulled off

---
+++ MSRP calc.py
@@ -0,0 +1,25 @@
+import sys
+def main():
+    retail()
+    repeat()
+def retail():
+#asks the end-user for a wholesale price
+ wholesale =(float(input("Enter wholesale price ")))
+
+#calculates said price to a retail MSRP
+ retailMSRP = wholesale * 2.50
+
+#gives out a price in said currency (such as $)
+ print("retail price is ",retailMSRP," $")
+ repeat()
+def repeat():
+    keepgoing =(input("do you wish to keep going Y/N "))
+
+    if keepgoing == "y":
+        retail()
+    else:
+     print("Goodbye...")
+     sys.exit()
+main()
+  
+
